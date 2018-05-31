773b7a83518a1e021b3feff5c1f07912cff9014f
Aaron <aaron@finsuite.com.au>
2018-05-31 17:51:57 +1000

    It's working!

--- test/websiteCheck.js
+++ test/websiteCheck.js
@@ -4,8 +4,8 @@ var assert = require('chai').assert
 const URL = 'http://127.0.0.1/bizanalyserservice/';
 var httpntlm = require('httpntlm');
 
-let webPages =[{name:'basePage',endURL:'',statusCode:'200',pass:''},
-    {name:'MRAServer',endURL:'mraserver',statusCode:'400',pass:''}]
+let webPages =[{name:'basePage',endURL:'http://127.0.0.1/bizanalyserservice/',statusCode:'200',pass:''},
+    {name:'MRAServer',endURL:'http://127.0.0.1/bizanalyserservice/mraserver2',statusCode:'404',pass:''}]
 
     var username ='aaron'
     var localMachineName ='WOMBAT';
@@ -13,36 +13,35 @@ let webPages =[{name:'basePage',endURL:'',statusCode:'200',pass:''},
     var encryptedNetworkPassword = new Buffer([ 21, 248, 36, 209, 102, 180, 83, 108, 76, 153, 246, 164, 249, 74, 105, 215 ]);
 
 describe('Test website behaviour per user', () =>{
-    var parameters =
-                {    
-                    url:URL,     
-                    username: username,
-                    lm_password: encryptedLocalMachinePassword,
-                    nt_password: encryptedNetworkPassword,
-                    workstation: localMachineName            
-                };  
-    let processTestData = function(data, callback){
-        for (let i=0; i<data.length;i++){
-            httpntlm.get(parameters,
+ 
+    let processTestData = function(iteration, callback ){
+        httpntlm.get({    
+            url:iteration.endURL,     
+            username: username,
+            lm_password: encryptedLocalMachinePassword,
+            nt_password: encryptedNetworkPassword,
+            workstation: localMachineName            
+        },            
                 function(err,response) 
                 {
                     if(err) console.log(err);
                     if(response){
-                           if(typeof callback === "function")
+                        assert.equal(response.statusCode, iteration.statusCode,'Status Code does not match');
+                        if(typeof callback === "function")
                                 {
-                                    callback(response,data[i]);
-                                }       
-                            } 
+                                    callback(response);
+                                } 
+                    } 
                 })     
-            }
-        }
-        it(`test`, (done) => {
-        processTestData(webPages,function(response,data){
-            console.log(response.statusCode); 
-            console.log(data.statusCode);
-            assert.equal(response.statusCode, data.statusCode,'Status Code does not match');
-            done();           
-            })
-        })
+            }           
 
-    })
+
+    for (let i=0; i<webPages.length;i++)
+    {  
+        it(`Test${i+1}: ${webPages[i].name} should return status code ${webPages[i].statusCode}`, (done) => {
+        processTestData(webPages[i],function(response){
+            done();
+        });
+        })
+    }
+})
