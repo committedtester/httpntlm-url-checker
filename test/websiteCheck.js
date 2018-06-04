fdf518f326e11807ed18910a20fd9a1342a12f2c
Aaron <aaron@finsuite.com.au>
2018-06-04 16:44:17 +1000

    Removal of obsolete files

--- test/websiteCheck.js
+++ test/websiteCheck.js
@@ -4,26 +4,19 @@ var assert = require('chai').assert
 const URL = 'http://127.0.0.1/bizanalyserservice/';
 var httpntlm = require('httpntlm');
 var websiteArray = require('../testData/websiteArray.js');
+var httpMethods = require('../helperMethods/httpMethods.js');
 
 let adminUserwebPages = websiteArray.adminUser;
+let regionAdminUserwebPages = websiteArray.regionAdminUser;
 
-
-
-/*
-let webPages =[
-    {name:'basePage',endURL:'http://127.0.0.1/bizanalyserservice/',statusCode:'200',pass:''},
-    {name:'MRAServer',endURL:'http://127.0.0.1/bizanalyserservice/mraserver',statusCode:'200',pass:''}
-
-]
-*/
-
-    var username ='aaron'
-    var localMachineName ='WOMBAT';
-    var encryptedLocalMachinePassword = new Buffer([ 43, 62, 12, 170, 200, 47, 245, 211, 105, 27, 138, 39, 153, 115, 76, 208 ]);
-    var encryptedNetworkPassword = new Buffer([ 21, 248, 36, 209, 102, 180, 83, 108, 76, 153, 246, 164, 249, 74, 105, 215 ]);
+var username ='aaron'
+var localMachineName ='WOMBAT';
+var encryptedLocalMachinePassword = new Buffer([ 43, 62, 12, 170, 200, 47, 245, 211, 105, 27, 138, 39, 153, 115, 76, 208 ]);
+var encryptedNetworkPassword = new Buffer([ 21, 248, 36, 209, 102, 180, 83, 108, 76, 153, 246, 164, 249, 74, 105, 215 ]);
 
 describe('Test website behaviour per user', () =>{
- 
+
+
     let processTestData = function(iteration, callback ){
         httpntlm.get({    
             url:iteration.endURL,     
--- testData/websiteArray.js
+++ testData/websiteArray.js
@@ -5,9 +5,9 @@ this.adminUser =
 {name:'Country',endURL: 'http://localhost/BizAnalyserService/Country',statusCode:'200'},	
 {name:'MraModel',endURL: 'http://localhost/BizAnalyserService/MraModel',statusCode:'200'},	
 {name:'AlternateFile',endURL: 'http://localhost/BizAnalyserService/AlternateFile',statusCode:'200'},	
-{name:'UserMapping_IdenticalItem',endURL: 'http://localhost/BizAnalyserService/UserMapping_IdenticalItem',statusCode:'200'},	
-{name:'IgnoreFID',endURL: 'http://localhost/BizAnalyserService/IgnoreFID',statusCode:'200'},	
-{name:'ExcludedWords',endURL: 'http://localhost/BizAnalyserService/ExcludedWords',statusCode:'200'},	
+{name:'Manage Alternates',endURL: 'http://localhost/BizAnalyserService/UserMapping_IdenticalItem',statusCode:'200'},	
+{name:'Excluded Categories',endURL: 'http://localhost/BizAnalyserService/IgnoreFID',statusCode:'200'},	
+{name:'Excluded KeyWords',endURL: 'http://localhost/BizAnalyserService/ExcludedWords',statusCode:'200'},	
 {name:'ItemNameChange',endURL: 'http://localhost/BizAnalyserService/ItemNameChange',statusCode:'200'},	
 {name:'AlternateFileRedirection',endURL: 'http://localhost/BizAnalyserService/AlternateFileRedirection',statusCode:'200'},	
 {name:'mraModel_FormulaFile',endURL: 'http://localhost/BizAnalyserService/mraModel_FormulaFile',statusCode:'200'},	
@@ -30,6 +30,38 @@ this.adminUser =
 {name:'ErrorLog',endURL: 'http://localhost/BizAnalyserService/ErrorLog',statusCode:'200'},	
 {name:'BzaLicense',endURL: 'http://localhost/BizAnalyserService/BzaLicense',statusCode:'200'}	
 ]
+
+this.regionAdminUser =
+[{name:'MraServer',endURL: 'http://localhost/BizAnalyserService/MraServer',statusCode:'401'},	
+{name:'Country',endURL: 'http://localhost/BizAnalyserService/Country',statusCode:'401'},	
+{name:'MraModel',endURL: 'http://localhost/BizAnalyserService/MraModel',statusCode:'401'},	
+{name:'AlternateFile',endURL: 'http://localhost/BizAnalyserService/AlternateFile',statusCode:'200'},	
+{name:'Manage Alternates',endURL: 'http://localhost/BizAnalyserService/UserMapping_IdenticalItem',statusCode:'200'},	
+{name:'Excluded Categories',endURL: 'http://localhost/BizAnalyserService/IgnoreFID',statusCode:'200'},	
+{name:'Excluded KeyWords',endURL: 'http://localhost/BizAnalyserService/ExcludedWords',statusCode:'200'},	
+{name:'ItemNameChange',endURL: 'http://localhost/BizAnalyserService/ItemNameChange',statusCode:'200'},	
+{name:'AlternateFileRedirection',endURL: 'http://localhost/BizAnalyserService/AlternateFileRedirection',statusCode:'401'},	
+{name:'mraModel_FormulaFile',endURL: 'http://localhost/BizAnalyserService/mraModel_FormulaFile',statusCode:'401'},	
+{name:'Formula',endURL: 'http://localhost/BizAnalyserService/Formula',statusCode:'401'},	
+{name:'Formula_Calculation',endURL: 'http://localhost/BizAnalyserService/Formula_Calculation',statusCode:'401'},	
+{name:'Template',endURL: 'http://localhost/BizAnalyserService/Template',statusCode:'200'},	
+{name:'ApplicationSettings',endURL: 'http://localhost/BizAnalyserService/ApplicationSettings',statusCode:'200'},	
+{name:'ExchangeRate',endURL: 'http://localhost/BizAnalyserService/ExchangeRate',statusCode:'401'},	
+{name:'ConfigurationSettings',endURL: 'http://localhost/BizAnalyserService/ConfigurationSettings',statusCode:'401'},	
+{name:'Batch',endURL: 'http://localhost/BizAnalyserService/Batch',statusCode:'200'},	
+{name:'BzaUser',endURL: 'http://localhost/BizAnalyserService/BzaUser',statusCode:'401'},	
+{name:'CountryAdmin',endURL: 'http://localhost/BizAnalyserService/CountryAdmin',statusCode:'200'},	
+{name:'CountryFolders',endURL: 'http://localhost/BizAnalyserService/CountryFolders',statusCode:'200'},	
+{name:'Section',endURL: 'http://localhost/BizAnalyserService/Section',statusCode:'401'},	
+{name:'SubSection',endURL: 'http://localhost/BizAnalyserService/SubSection',statusCode:'401'},	
+{name:'FinancialData',endURL: 'http://localhost/BizAnalyserService/FinancialData',statusCode:'401'},	
+{name:'CustomerPeriodUpdates',endURL: 'http://localhost/BizAnalyserService/CustomerPeriodUpdates',statusCode:'200'},	
+{name:'Report',endURL: 'http://localhost/BizAnalyserService/Report',statusCode:'200'},	
+{name:'AccessLog',endURL: 'http://localhost/BizAnalyserService/AccessLog',statusCode:'401'},	
+{name:'ErrorLog',endURL: 'http://localhost/BizAnalyserService/ErrorLog',statusCode:'401'},	
+{name:'BzaLicense',endURL: 'http://localhost/BizAnalyserService/BzaLicense',statusCode:'401'}	
+]
+
 }
 
 module.exports = new webSiteArray();
