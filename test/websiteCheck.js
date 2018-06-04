7933883b9bd56edc7580e5dd75bfeabbd968cd04
Aaron <aaron@finsuite.com.au>
2018-06-04 21:36:50 +1000

    Removal of unneccessary code

--- test/websiteCheck.js
+++ test/websiteCheck.js
@@ -1,18 +1,14 @@
-;
 var chai = require('chai');  
 var assert = require('chai').assert
-//const URL = 'http://127.0.0.1/bizanalyserservice/';
 var httpntlm = require('httpntlm');
 var websiteArray = require('../testData/websiteArray.js');
 var os = require("os");
 
 let adminUserwebPages = websiteArray.adminUser;
-let regionAdminUserwebPages = websiteArray.regionAdminUser;
-
-var username =os.userInfo().username
-var localMachineName =os.hostname();
-var encryptedLocalMachinePassword = new Buffer([ 43, 62, 12, 170, 200, 47, 245, 211, 105, 27, 138, 39, 153, 115, 76, 208 ]);
-var encryptedNetworkPassword = new Buffer([ 21, 248, 36, 209, 102, 180, 83, 108, 76, 153, 246, 164, 249, 74, 105, 215 ]);
+let username =os.userInfo().username
+let localMachineName =os.hostname();
+let encryptedLocalMachinePassword = new Buffer([ 43, 62, 12, 170, 200, 47, 245, 211, 105, 27, 138, 39, 153, 115, 76, 208 ]);
+let encryptedNetworkPassword = new Buffer([ 21, 248, 36, 209, 102, 180, 83, 108, 76, 153, 246, 164, 249, 74, 105, 215 ]);
 
 describe('Test website behaviour for admin user', () =>{
 
