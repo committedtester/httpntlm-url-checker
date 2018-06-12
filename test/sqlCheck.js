a6afd5f1c24b90802ba147db53f704652e02bb10
Aaron McKay <aaronmckaynz@gmail.com>
2018-06-13 08:31:38 +1000

    Removal of unnecessary require

--- test/sqlCheck.js
+++ test/sqlCheck.js
@@ -1,13 +1,8 @@
-var Connection = require('tedious').Connection;
-var Request = require('tedious').Request;
-//var async = require('async');
 var sqlMethods =require('../helperMethods/sql');
-
 
 describe('Test sqldatabase', () =>{  
  
-    it.only(`Check mssql package works`, () => {  
-
+    it.only(`Check mssql package works`, () => { 
     var countCustomers ='select count(*) from dbo.FinancialData1404';  
     sqlMethods.sqlRead(countCustomers);
 
