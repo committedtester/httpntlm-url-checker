d45b10fe1635983f3278688768a2bf376bebd16d
Aaron McKay <aaronmckaynz@gmail.com>
2018-06-14 22:47:17 +1000

    Working URL checker

--- helperMethods/http.js
+++ helperMethods/http.js
@@ -8,7 +8,6 @@ let localMachineName =os.hostname();
 let encryptedLocalMachinePassword = new Buffer([ 43, 62, 12, 170, 200, 47, 245, 211, 105, 27, 138, 39, 153, 115, 76, 208 ]);
 let encryptedNetworkPassword = new Buffer([ 21, 248, 36, 209, 102, 180, 83, 108, 76, 153, 246, 164, 249, 74, 105, 215 ]);
 
-
 var http = function() {
 
     this.processTestData = function(iteration,callback ){
@@ -23,11 +22,7 @@ var http = function() {
                 {
                     if(err) console.log(err);
                     if(response){
-                        assert.equal(response.statusCode, iteration.statusCode,'Status Code does not match');
-                        if(typeof callback === "function")
-                                {
                                     callback(response);
-                                } 
                     } 
                 })     
             } 
--- test/websiteCheck.js
+++ test/websiteCheck.js
@@ -2,12 +2,16 @@ var http =require('../helperMethods/http');
 var websiteArray = require('../testData/websiteArray.js');
 let adminUserwebPages = websiteArray.adminUser;
 
+var chai = require('chai');  
+var assert = require('chai').assert
+
 describe('Test website behaviour for admin user', () =>{
     for (let i=0; i<adminUserwebPages.length;i++)
     {  
         it(`Test${i+1}: ${adminUserwebPages[i].name} should return status code ${adminUserwebPages[i].statusCode}`, (done) => {
         http.processTestData(adminUserwebPages[i],function(response){
-            done();
+            assert.equal(response.statusCode, adminUserwebPages[i].statusCode,'Status Code does not match');
+            done();                 
         });
         })
     }
